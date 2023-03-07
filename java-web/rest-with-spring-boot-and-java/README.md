f2e4aa150ef8e0c7217e45a80627634651e9e112
Ygor Simões <contato.ygorsimoes@gmail.com>
2023-03-07 12:16:57 -0300

    Added two projects

---
+++ java-web/rest-with-spring-boot-and-java/README.md
@@ -0,0 +1,3 @@
+## ☕ REST API with Spring Boot 3 and Java 19
+
+This is a sample project to demonstrate how to build a REST API with Spring Boot 3 and Java 19 where I also used AWS and Docker for infrastructure and Cloud.
---
+++ java-web/rest-with-spring-boot-and-java/src/test/java/com/ygorsimoes/StartupTests.java
@@ -0,0 +1,13 @@
+package com.ygorsimoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
