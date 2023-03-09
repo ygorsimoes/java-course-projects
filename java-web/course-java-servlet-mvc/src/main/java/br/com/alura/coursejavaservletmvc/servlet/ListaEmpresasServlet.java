33bb47f3c65c58c2e83c5abb6ae95b3155763ea0
Ygor Simões <contato.ygorsimoes@gmail.com>
2023-03-09 09:15:23 -0300

    Minor updates

--- java-web/course-java-servlet-mvc/src/main/java/br/com/alura/coursejavaservletmvc/servlet/ListaEmpresasServlet.java
+++ java-web/course-java-servlet-mvc/src/main/java/br/com/alura/coursejavaservletmvc/servlet/ListaEmpresasServlet.java
@@ -3,30 +3,29 @@ package br.com.alura.coursejavaservletmvc.servlet;
 import br.com.alura.coursejavaservletmvc.modelo.Banco;
 import br.com.alura.coursejavaservletmvc.modelo.Empresa;
 
-import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/listaEmpresas")
 public class ListaEmpresasServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	protected void service(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		
-		Banco banco = new Banco();
-		List<Empresa> lista = banco.getEmpresas();
-		
-		request.setAttribute("empresas", lista);
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
-		rd.forward(request, response);
-		
-	}
+    private static final long serialVersionUID = 1L;
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Banco banco = new Banco();
+        List<Empresa> lista = banco.getEmpresas();
+
+        request.setAttribute("empresas", lista);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+        rd.forward(request, response);
+
+    }
 }
