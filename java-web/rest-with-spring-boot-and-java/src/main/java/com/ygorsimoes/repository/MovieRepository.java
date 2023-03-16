33a61b1b8571f1d5c3f069de4421e78535c6f00d
Ygor Simões <contato.ygorsimoes@gmail.com>
2023-03-16 14:33:31 -0300

    New project in "java-web"
    Name: rest-api-with-spring-boot-and-java
    Course: https://www.udemy.com/course/restful-apis-do-0-a-nuvem-com-springboot-e-docker

--- java-web/rest-with-spring-boot-and-java/src/main/java/com/ygorsimoes/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ygorsimoes.repository;
-
-import com.ygorsimoes.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> { }
